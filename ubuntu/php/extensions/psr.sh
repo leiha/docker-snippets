8783de208f8bdb4f0b677c10c47e70dc9fed1ba3
Leiha <leiha.xyz@gmail.com>
2019-04-10 20:37:01 +0200

    Update psr.sh

--- ubuntu/php/extensions/psr.sh
+++ ubuntu/php/extensions/psr.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-pecl install psr 
-    && touch /usr/local/etc/php/conf.d/docker-php-ext-psr.ini \
-        && echo "extension=/usr/local/lib/php/extensions/no-debug-non-zts-20170718/psr.so" \                           
-            >> /usr/local/etc/php/conf.d/docker-php-ext-psr.ini
+git clone https://github.com/jbboehr/php-psr.git \
+    && cd ./php-psr \
+    && phpize \
+    && ./configure \
+    && make \
+    && make test \
+    && make install \
+    && cd .. \
+    && rm -rf ./php-psr \
+    && docker-php-ext-enable psr.so
