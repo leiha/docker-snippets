e2487092895925b242d8386dcb9d45594cc2d275
Leiha <leiha.xyz@gmail.com>
2019-04-10 20:29:17 +0200

    Update phalcon.sh

--- ubuntu/php/extensions/phalcon.sh
+++ ubuntu/php/extensions/phalcon.sh
@@ -2,14 +2,14 @@
 
 apt-get install -y libpcre3-dev gcc make re2c
 
-git clone git://github.com/phalcon/cphalcon.git --branch=master
-cd cphalcon/build
-sudo ./install
+git clone --single-branch --branch 3.4.x git://github.com/phalcon/cphalcon.git \
+    && cd cphalcon/build \
+    && ./install \
+    && cd ../.. \
+    && rm -rf ./cphalcon \
+    && touch /usr/local/etc/php/conf.d/docker-php-ext-phalcon.ini \
+        && echo "extension=/usr/local/lib/php/extensions/no-debug-non-zts-20170718/phalcon.so" \
+            >> /usr/local/etc/php/conf.d/docker-php-ext-phalcon.ini
 
-cd ../..
 
-rm -rf ./cphalcon
 
-touch /usr/local/etc/php/conf.d/docker-php-ext-phalcon.ini \
-    && echo "extension=/usr/local/lib/php/extensions/no-debug-non-zts-20170718/phalcon.so" \
-    >> /usr/local/etc/php/conf.d/docker-php-ext-phalcon.ini
