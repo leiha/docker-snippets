7f4e5c1159068c9c175f0726f6e11497716d730f
Leiha <leiha.xyz@gmail.com>
2019-04-17 10:09:52 +0200

    Update install.sh

--- ubuntu/node/install.sh
+++ ubuntu/node/install.sh
@@ -5,7 +5,7 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${basedir}
 
 apt-get install -y gnupg apt-transport-https \
-  && curl -sL https://deb.nodesource.com/setup_9.x | bash -
+  && curl -sL https://deb.nodesource.com/setup_9.x | bash - \
   && apt-get update \
   && apt-get install -y nodejs
   
