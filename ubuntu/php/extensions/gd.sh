4342b507dd2c9d82df3cdd03067fb0bac0c27c7d
Leiha <leiha.xyz@gmail.com>
2019-04-17 08:45:51 +0200

    Update gd.sh

--- ubuntu/php/extensions/gd.sh
+++ ubuntu/php/extensions/gd.sh
@@ -1,5 +1,3 @@
-apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libpng-dev \ 
-    && docker-php-ext-configure gd \
-        --with-freetype-dir=/usr/include/ \
-        --with-jpeg-dir=/usr/include/ \
-    && docker-php-ext-install gd
+apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libpng-dev 
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+docker-php-ext-install gd
