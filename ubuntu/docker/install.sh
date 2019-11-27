1b415e3a3c83197929ca7b4d17304689b61d6cad
leiha <leiha.sellier@trucksme.com>
2019-11-27 09:08:15 +0100

    add docker install

---
+++ ubuntu/docker/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+prevdir=$PWD
+basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${basedir}
+
+# ------
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker ${UID}
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+cd prevdir
